146dde1edac75e1a3418b84444e3d240cd3836ee
RaviTejaVattem <ravitejavattem@gmail.com>
2019-10-08 18:54:27 +0530

    watch route change with popstate and imperative events

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule, Injectable } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { Routes, RouterModule, Router, NavigationStart } from "@angular/router";
+import { Event as NavigationEvent } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: "my-app",
@@ -10,7 +12,52 @@ import { Routes, RouterModule } from "@angular/router";
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {
+    router.events
+      .pipe(
+        // The "events" stream contains all the navigation events. For this demo,
+        // though, we only care about the NavigationStart event as it contains
+        // information about what initiated the navigation sequence.
+        filter((event: NavigationEvent) => {
+          return event instanceof NavigationStart;
+        })
+      )
+      .subscribe((event: NavigationStart) => {
+        console.group("NavigationStart Event");
+        // Every navigation sequence is given a unique ID. Even "popstate"
+        // navigations are really just "roll forward" navigations that get
+        // a new, unique ID.
+        console.log("navigation id:", event.id);
+        console.log("route:", event.url);
+        // The "navigationTrigger" will be one of:
+        // --
+        // - imperative (ie, user clicked a link).
+        // - popstate (ie, browser controlled change such as Back button).
+        // - hashchange
+        // --
+        // NOTE: I am not sure what triggers the "hashchange" type.
+        console.log("trigger:", event.navigationTrigger);
+
+        // This "restoredState" property is defined when the navigation
+        // event is triggered by a "popstate" event (ex, back / forward
+        // buttons). It will contain the ID of the earlier navigation event
+        // to which the browser is returning.
+        // --
+        // CAUTION: This ID may not be part of the current page rendering.
+        // This value is pulled out of the browser; and, may exist across
+        // page refreshes.
+        if (event.restoredState) {
+          console.warn(
+            "restoring navigation id:",
+            event.restoredState.navigationId
+          );
+        }
+
+        console.groupEnd();
+      });
+  }
+}
 
 @Component({
   selector: "app-a",
@@ -35,7 +82,7 @@ export class CComponent {}
 @Injectable()
 export class serviceName {
   canDeactivate(component) {
-    console.log('>>>>>>>>>>>>>>>>> canDeactivate called')
+    console.log(">>>>>>>>>>>>>>>>> canDeactivate called");
     return false;
   }
 }
